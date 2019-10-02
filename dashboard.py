833e5b792b4e8e325778fe28d1a05db7471554b4
kinsley <ndengekinsley@gmail.com>
2019-10-02 07:59:48 +0200

    UI Updates with Cards V1

--- dashboard.py
+++ dashboard.py
@@ -51,84 +51,133 @@ colors = {'background':'#a7cef2', 'text':'#7FDBFF'}
 
 #Use dash html components to diplay html on dash
 app.layout = html.Div(children=[
-             html.H1("Hello Kedumetse!"),
+             dcc.Input(
+                    id="my_name",
+                    type='text',
+                    value='Enter your name',
+                    style = {'display': 'inline-block', 'width': '30%', 'margin':'auto','font-size':'150%'}
+                ),
+             html.Div(id="my_newname", style={"border-width": "10px",'width': '30%','font-size':'150%'}),
+             html.Br(),
              dcc.Markdown(children=markdown_text, style={"border-style":'solid',"text-align":'center'}),
              html.Br(),
 
- # Div that creates a Navigation tab in dasbboard
+ # Div that creates tabs with a summary of student submissions
             html.Div(
-            [
+                        [
                 html.H2("Below is an Summary of your assignment submissions"),
                 html.Br(),
-                dbc.Tab(
-                        label="Overview",
-                        style={"padding": "10px"},
-                        ),
-                            dbc.Card(
-                                dbc.CardBody(
-                                    [
-                                        html.P(
-                                            "Click Here to View Output from Submitted Assignments",
-                                            className="card-text",
-                                        ),
-                                        dbc.Button("Click here", color="success",id="view_output"),
-                                        html.Div(id="output_text"),
-                                    ]
-                                )
-                            ),
-                        ]),
-
-#Row that displays different data to user
-            html.Div([
-                            html.Div(
-                                [html.H6(id="countdownText"), html.P("Time Till Next Dateline: 13 Days")],
-                                id="countdown",
-                                className="mini_container",
-                                style={"padding": "20px", 'color':'#F40009'},
-                            ),
-                                html.Div(
-                                    [html.H6(id="highestscoreText"), html.P("Your Highest Score: 80%")],
-                                    id="highestscore",
-                                    className="mini_container",
-                                    style={"padding": "20px", 'color':'#F40009'},
-                            ),
-                                html.Div(
-                                    [html.H6(id="averagescoreText"), html.P("Your Average Score: 78%")],
-                                    id="averagescore",
-                                    className="mini_container",
-                                    style={"padding": "20px", 'color':'#F40009'},
-                            ),
-                                html.Div(
-                                    [html.H6(id="classaveragescoreText"), html.P("Class Average Score: 72%")],
-                                    id="classaveragescore",
-                                    className="mini_container",
-                                    style={"padding": "20px", 'color':'#F40009'},
-                            ),
-                                html.Div(
-                                    [html.H6(id="rateText"), html.P("Rate of Assignment Submission Per Hour: 3")],
-                                    id="ratescore",
-                                    className="mini_container",
-                                    style={"padding": "20px", 'color':'#F40009'},
-                            ),
-                                    html.Div(
-                                        [html.H6(id="errorsText"), html.P("Common Errors: Type Error")],
-                                        id="commonerrors",
-                                        className="mini_container",
-                                        style={"padding": "20px", 'color':'#F40009'},
+                    dbc.Row([
+                        dbc.Col(
+                        dbc.Card(
+                                    dbc.CardBody(
+                                        [
+                                            html.P(
+                                                "Output from Submitted Assignments",
+                                                className="card-text",
+                                            ),
+                                            dbc.CardLink("Click here", href="https://vula.uct.ac.za/portal"),
+                                        ]
+                                    ),
+                                    style={"width": "18rem", 'color':'#F40009'},
                                 ),
-                                html.Div(
-                                    [html.H6(id="dpneededText"), html.P("DP Needed: 67%")],
-                                    id="dpneeded",
-                                    className="mini_container",
-                                    style={"padding": "20px", 'color':'#F40009'},
-                            ),
-                             ],
-                                 id="info-container",
-                                 className="row container-display",
-                                 style={"align": "center"},
-
-                            ),
-
+                                ),
+                        dbc.Col(
+                        dbc.Card(
+                                    dbc.CardBody(
+                                        [
+                                            html.P(
+                                                "Next Due Date: 13 Days",
+                                                className="card-text",
+                                            ),
+                                        ]
+                                    ),
+                                    style={"width": "18rem"},
+                                ),
+                                ),
+                        dbc.Col(
+                        dbc.Card(
+                                    dbc.CardBody(
+                                        [
+                                            html.P(
+                                                "Your Average Score: 78%",
+                                                className="card-text",
+                                            ),
+                                        ]
+                                    ),
+                                    style={"width": "18rem"},
+                                ),
+                                ),
+                        dbc.Col(
+                        dbc.Card(
+                                    dbc.CardBody(
+                                        [
+                                            html.P(
+                                                "Class Average Score: 72%",
+                                                className="card-text",
+                                            ),
+                                        ]
+                                    ),
+                                    style={"width": "18rem"},
+                                ),
+                                ),
+                            ]),
+                        dbc.Row([
+                            dbc.Col(
+                            dbc.Card(
+                                        dbc.CardBody(
+                                            [
+                                                html.P(
+                                                    "Rate of Assignment Submission Per Hour: 3",
+                                                    className="card-text",
+                                                ),
+                                            ]
+                                        ),
+                                        style={"width": "18rem"},
+                                    ),
+                                    ),
+                            dbc.Col(
+                            dbc.Card(
+                                        dbc.CardBody(
+                                            [
+                                                html.P(
+                                                    "Common Errors: Type Error",
+                                                    className="card-text",
+                                                ),
+                                            ]
+                                        ),
+                                        style={"width": "18rem"},
+                                    ),
+                                    ),
+                            dbc.Col(
+                            dbc.Card(
+                                        dbc.CardBody(
+                                            [
+                                                html.P(
+                                                    "DP Needed: 67%",
+                                                    className="card-text",
+                                                ),
+                                            ]
+                                        ),
+                                        style={"width": "18rem"},
+                                    ),
+                                    ),
+                                dbc.Col(
+                                dbc.Card(
+                                            dbc.CardBody(
+                                                [
+                                                    html.P(
+                                                        "Your Highest Score: 80%",
+                                                        className="card-text",
+                                                    ),
+                                                ]
+                                            ),
+                                            style={"width": "18rem"},
+                                        ),
+                                        ),
+                                ]),
+                        ]),
+                        html.Br(),
 
     #create a dropdown that menu item
             dcc.Dropdown(id='mark_picker',
@@ -161,6 +210,15 @@ app.layout = html.Div(children=[
 
 
             )], style={'margin':'120px'})
+
+#Callbacks to update Dashboard
+
+#Callback to update output div for student name
+@app.callback(Output(component_id="my_newname" , component_property="children"),
+                    [Input(component_id="my_name", component_property="value")])
+def update_name_output(input_value):
+    return "Welcome: {}".format(input_value)
+
 
 if __name__ == '__main__':
     app.run_server()
